80f2db4d651a56299bce1700c2ba557b59879e9e
Luke Bollwerk <lukas8@gmail.com>
2025-04-22 07:01:02 -0600

    let me see your id

---
+++ library/src/main/kotlin/klutch/db/tables/UserDtoAspect.kt
@@ -0,0 +1,29 @@
+package klutch.db.tables
+
+import klutch.db.Aspect
+import klutch.utils.toInstantUtc
+import kabinet.model.UserRole
+import kabinet.model.User
+import org.jetbrains.exposed.sql.ResultRow
+
+object UserDtoAspect : Aspect<UserDtoAspect, User>(
+    UserTable,
+    ResultRow::toUserDto
+) {
+    val id = add(UserTable.id)
+    val username = add(UserTable.username)
+    val roles = add(UserTable.roles)
+    val avatarUrl = add(UserTable.avatarUrl)
+    val createdAt = add(UserTable.createdAt)
+    val updatedAt = add(UserTable.updatedAt)
+}
+
+fun ResultRow.toUserDto() = User(
+    id = this[UserDtoAspect.id].value,
+    username = this[UserDtoAspect.username],
+    roles = this[UserDtoAspect.roles].map { UserRole.valueOf(it) }.toSet(),
+    avatarUrl = this[UserDtoAspect.avatarUrl],
+    createdAt = this[UserDtoAspect.createdAt].toInstantUtc(),
+    updatedAt = this[UserDtoAspect.updatedAt].toInstantUtc(),
+)
+
