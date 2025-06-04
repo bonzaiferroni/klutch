30a03fe0c6d5f37d6b34fdfd4321076c547d9364
Luke Bollwerk <lukas8@gmail.com>
2025-06-04 14:30:01 -0600

    move promptTemplate

---
+++ library/src/main/kotlin/klutch/clients/promptTemplate.kt
@@ -0,0 +1,21 @@
+package klutch.clients
+
+import java.io.File
+
+fun promptTemplate(
+    path: String,
+    vararg items: Pair<String, String>
+): String {
+    val text = File(path).readText()
+    val sb = StringBuilder(text)
+    items.forEach { (key, value) ->
+        val templateKey = "<|$key|>"
+        do {
+            var index = sb.indexOf(templateKey)
+            if (index >= 0)  {
+                sb.replace(index, index + templateKey.length, value)
+            }
+        } while (index >= 0)
+    }
+    return sb.toString()
+}
