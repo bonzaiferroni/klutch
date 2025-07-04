0db3ef9f2b2a729daa3810ee8db1f2c8c113f49a
Luke Bollwerk <lukas8@gmail.com>
2025-07-04 08:24:56 -0600

    make sync more robust

--- library/src/main/kotlin/klutch/utils/ExpressionFunctions.kt
+++ library/src/main/kotlin/klutch/utils/ExpressionFunctions.kt
@@ -15,14 +15,29 @@ import kotlin.time.Duration
 fun ExpressionWithColumnType<LocalDateTime>.since(duration: Duration) = duration.let { (Clock.System.now() - it).toLocalDateTimeUtc() }
     .let { Op.build { this@since.greater(it) } }
 
+fun ExpressionWithColumnType<LocalDateTime>.greaterEq(instant: Instant) =
+    Op.build { this@greaterEq.greaterEq(instant.toLocalDateTimeUtc()) }
+
+fun ExpressionWithColumnType<LocalDateTime?>.greaterEqNullable(instant: Instant) =
+    Op.build { this@greaterEqNullable.greaterEq(instant.toLocalDateTimeUtc()) }
+
 fun ExpressionWithColumnType<LocalDateTime>.greater(instant: Instant) =
     Op.build { this@greater.greater(instant.toLocalDateTimeUtc()) }
+
+fun ExpressionWithColumnType<LocalDateTime?>.greaterNullable(instant: Instant) =
+    Op.build { this@greaterNullable.greater(instant.toLocalDateTimeUtc()) }
 
 fun ExpressionWithColumnType<LocalDateTime>.less(instant: Instant) =
     Op.build { this@less.less(instant.toLocalDateTimeUtc()) }
 
+fun ExpressionWithColumnType<LocalDateTime?>.lessNullable(instant: Instant) =
+    Op.build { this@lessNullable.less(instant.toLocalDateTimeUtc()) }
+
 fun ExpressionWithColumnType<LocalDateTime>.lessEq(instant: Instant) =
     Op.build { this@lessEq.lessEq(instant.toLocalDateTimeUtc()) }
+
+fun ExpressionWithColumnType<LocalDateTime?>.lessEqNullable(instant: Instant) =
+    Op.build { this@lessEqNullable.lessEq(instant.toLocalDateTimeUtc()) }
 
 fun <T> ExpressionWithColumnType<T>.isNullOrEq(t: T) =
     Op.build { this@isNullOrEq.isNull() or this@isNullOrEq.eq(t) }
