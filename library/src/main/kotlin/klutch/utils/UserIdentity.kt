9742bbadc19413039d962cede005b7c9acfadaea
bonzaiferroni <lukas8@gmail.com>
2026-04-25 08:25:46 -0600

    iterate on comments

--- library/src/main/kotlin/klutch/utils/UserIdentity.kt
+++ library/src/main/kotlin/klutch/utils/UserIdentity.kt
@@ -1,5 +1,6 @@
 package klutch.utils
 
+import io.ktor.server.application.ApplicationCall
 import io.ktor.server.auth.jwt.JWTPrincipal
 import io.ktor.server.auth.principal
 import io.ktor.server.routing.RoutingCall
@@ -18,32 +19,32 @@ data class UserIdentity<Id: AuthId>(val userId: Id, val username: String, val ro
 class Identity<User: AuthUser, Id: AuthId>(
     private val dao: AuthDao<User, Id>
 ) {
-    suspend fun getIdentityOrNull(call: RoutingCall): UserIdentity<Id>? {
+    suspend fun getIdentityOrNull(call: ApplicationCall): UserIdentity<Id>? {
         val username = getClaimOrNull(call, CLAIM_USERNAME) ?: return null
         val roles = getClaimOrNull(call, CLAIM_ROLES)?.toUserRoleSet() ?: return null
         val userId = dao.readIdByUsername(username) ?: return null
         return UserIdentity(userId, username, roles)
     }
 
-    suspend fun getIdentity(call: RoutingCall) = getIdentityOrNull(call) ?: error("user identity not found")
+    suspend fun getIdentity(call: ApplicationCall) = getIdentityOrNull(call) ?: error("user identity not found")
 
-    suspend fun getUserIdOrNull(call: RoutingCall) = getIdentityOrNull(call)?.userId
+    suspend fun getUserIdOrNull(call: ApplicationCall) = getIdentityOrNull(call)?.userId
 
-    suspend fun getUserId(call: RoutingCall) = getUserIdOrNull(call) ?: error("user id not provided")
+    suspend fun getUserId(call: ApplicationCall) = getUserIdOrNull(call) ?: error("user id not provided")
 
-    fun getUsernameOrNull(call: RoutingCall): String? {
+    fun getUsernameOrNull(call: ApplicationCall): String? {
         return getClaimOrNull(call, CLAIM_USERNAME)
     }
 
-    fun getUsername(call: RoutingCall): String {
+    fun getUsername(call: ApplicationCall): String {
         return getClaimOrNull(call, CLAIM_USERNAME) ?: error("username not provided")
     }
 
-    fun testRole(call: RoutingCall, role: String): Boolean {
+    fun testRole(call: ApplicationCall, role: String): Boolean {
         return getClaimOrNull(call, CLAIM_ROLES)?.contains(role) ?: false
     }
 
-    fun getClaimOrNull(call: RoutingCall, name: String): String? {
+    fun getClaimOrNull(call: ApplicationCall, name: String): String? {
         return call.principal<JWTPrincipal>()?.payload?.getClaim(name)?.asString()
     }
 }
