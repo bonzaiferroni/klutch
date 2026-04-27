597061e74d949fdca86780525d86658252d0669c
bonzaiferroni <lukas8@gmail.com>
2026-04-27 06:37:50 -0600

    refactor posts

--- library/src/main/kotlin/klutch/db/DbService.kt
+++ library/src/main/kotlin/klutch/db/DbService.kt
@@ -1,15 +1,24 @@
 package klutch.db
 
 import kotlinx.coroutines.Dispatchers
+import org.jetbrains.exposed.v1.core.StdOutSqlLogger
 import org.jetbrains.exposed.v1.core.Transaction
 import org.jetbrains.exposed.v1.jdbc.transactions.experimental.newSuspendedTransaction
 import org.jetbrains.exposed.v1.jdbc.transactions.suspendTransaction
 
 abstract class DbService(
-    val defaultMaxAttempts: Int = 1
+    val defaultMaxAttempts: Int = 1,
+    val defaultAddLogger: Boolean = false,
 ) {
-    suspend fun <T> dbQuery(maxAttempts: Int = defaultMaxAttempts, block: suspend Transaction.() -> T): T =
+    suspend fun <T> dbQuery(
+        addLogger: Boolean = defaultAddLogger,
+        maxAttempts: Int = defaultMaxAttempts,
+        block: suspend Transaction.() -> T
+    ): T =
         suspendTransaction {
+            if (addLogger) {
+                addLogger(StdOutSqlLogger)
+            }
             this.maxAttempts = maxAttempts
             block()
         }
