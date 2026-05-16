c1036fc87ba68ba8eb8887b4326fe1d942a45073
bonzaiferroni <lukas8@gmail.com>
2026-05-16 13:08:00 -0600

    added GeoBounds to locality

--- library/src/main/kotlin/klutch/utils/GeoPointFunctions.kt
+++ library/src/main/kotlin/klutch/utils/GeoPointFunctions.kt
@@ -1,7 +1,10 @@
 package klutch.utils
 
+import kampfire.model.GeoBounds
 import kampfire.model.GeoPoint
 import org.postgresql.geometric.PGpoint
 
 fun GeoPoint.toPGpoint() = PGpoint(x, y)
 fun PGpoint.toGeoPoint() = GeoPoint(x, y)
+fun List<Double>.toGeoBounds() = GeoBounds(GeoPoint(this[0], this[1]), GeoPoint(this[2], this[3]))
+fun GeoBounds.toList() = listOf(sw.lng, sw.lat, ne.lng, ne.lat)
