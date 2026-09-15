b2bc6a35d759d7cfec7a084b4183a0f7bffaa13a
bonzaiferroni <lukas8@gmail.com>
2026-09-15 14:37:18 -0600

    added sops

--- library/src/main/kotlin/klutch/environment/environment.kt
+++ library/src/main/kotlin/klutch/environment/environment.kt
@@ -8,3 +8,11 @@ fun readEnvFromPathOrNull(path: String = "../.env") = File(path)
     ?.let { Environment.fromText(it.readText()) }
 
 fun readEnvFromPath(path: String = "../.env") = readEnvFromPathOrNull(path) ?: error("Unable to find .env at $path")
+
+class SystemEnvironment(private val fallback: Environment?): Environment {
+    override fun readOrNull(key: String) = System.getenv(key) ?: fallback?.readOrNull(key)
+
+    companion object {
+        fun fromSystem(fallback: Environment? = null): Environment = SystemEnvironment(fallback)
+    }
+}
